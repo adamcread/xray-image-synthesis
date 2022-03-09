25710b91c498024bb5f4397d2ebef6428504f04c
adamcread <adamcread1@gmail.com>
2022-03-09 15:30:09 +0000

    ssim first try

--- compositionGAN/models/objCompose_unsupervised_model.py
+++ compositionGAN/models/objCompose_unsupervised_model.py
@@ -15,6 +15,7 @@ from . import networks
 from torch import index_select, LongTensor, nn
 from scipy.ndimage.morphology import binary_erosion
 import torch.nn.functional as F
+import pytorch_ssim
 
 
 class objComposeUnsuperviseModel(BaseModel):
@@ -176,6 +177,8 @@ class objComposeUnsuperviseModel(BaseModel):
             # ----------------------------------
             self.criterionGAN = networks.GANLoss(use_lsgan=not opt.no_lsgan, tensor=self.Tensor)
             self.criterionL1 = torch.nn.L1Loss()
+            self.ssim_loss = pytorch_ssim.SSIM()
+
             self.criterionCLS = nn.CrossEntropyLoss()
             self.criterionbCLS = nn.BCELoss()
 
@@ -517,9 +520,12 @@ class objComposeUnsuperviseModel(BaseModel):
     def backward_STN(self):
         '''backward pass for training STN networks only'''
         # self.loss_STN = (self.criterionL1(self.stn_B1, self.real_B1) + self.criterionL1(self.stn_B2, self.real_B2))
-        self.loss_STN = 50*(self.criterionL1(self.stn_B2, self.real_B2))
+        # self.loss_STN = 50*(self.criterionL1(self.stn_B2, self.real_B2))
         # self.loss_STN += 100*(self.criterionL1(self.stn_B1_T, self.real_B1_T) + self.criterionL1(self.stn_B2_T, self.real_B2_T))
-        self.loss_STN += 50*(self.criterionL1(self.stn_B2_T, self.real_B2_T))
+        # self.loss_STN += 50*(self.criterionL1(self.stn_B2_T, self.real_B2_T))
+
+        self.loss_STN = (2 - self.ssim_loss(self.stn_B1, self.real_B1) - self.ssim_loss(self.stn_B2, self.real_B2))
+        self.loss_STN = 100*(2 - self.ssim_loss(self.stn_B1_T, self.real_B1_T) - self.ssim_loss(self.stn_B2_T, self.real_B2_T)
         self.loss_STN.backward()
 
     def backward_G_completion(self):
