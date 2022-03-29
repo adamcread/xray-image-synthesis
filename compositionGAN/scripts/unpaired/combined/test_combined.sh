21458d0b33ac53f85d4ba1bc77d38f5d15bbe500
adamcread <adamcread1@gmail.com>
2022-03-29 17:40:09 +0100

    skjdcs

--- compositionGAN/scripts/unpaired/combined/test_combined.sh
+++ compositionGAN/scripts/unpaired/combined/test_combined.sh
@@ -1,16 +1,37 @@
+#!/bin/bash
+# X number of nodes with Y number of cores in each node.
+#SBATCH -N 1
+#SBATCH -c 4
+
+# partition time limit and resource limit for the job
+#SBATCH --gres=gpu
+#SBATCH -p ug-gpu-small	
+#SBATCH --mem=28g
+#SBATCH --qos=long-high-prio
+#SBATCH -t 07-00:00:00
+#SBATCH -o 'unpaired_combined_dice_batch.txt'
+
+# job name
+#SBATCH --job-name=unpaired_combined
+
+# Source the bash profile (required to use the module command)
+source /etc/profile
+module load cuda/11.0-cudnn8.0
+source ../venv/bin/activate
+
 mode=test
 name="test"
 
 datalist="./scripts/unpaired/threat_mask/train_comb_paths.txt"
 datalist_test="./scripts/unpaired/test.txt"
 
-exp_name="test_unpaired_combined_focal_tversky_700"
-name_train="test_unpaired_combined_focal_tversky_700"
+exp_name="test_unpaired_combined_focal_tversky_900"
+name_train="test_unpaired_combined_focal_tversky_900"
 model_train="./checkpoints/${name_train}" 
 
 name="${exp_name}"
 dataset_mode='comp_decomp_unaligned'
-how_many=640
+how_many=20000
 batch_size=1
 loadSizeY=128
 fineSizeY=128
