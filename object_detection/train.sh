2cbfe626d26740fd907f94a5d75a3dab91110151
adamcread <adamcread1@gmail.com>
2022-03-31 10:21:53 +0100

    typo

--- object_detection/train.sh
+++ object_detection/train.sh
@@ -23,6 +23,6 @@ source ../venv/bin/activate
 python3 tools/train.py './configs/custom/cascade_rcnn_config.py' \
     --cfg-options   "work_dir=work_dir/train/$1" \
                     "data.train.img_prefix=$2" \
-                    "data.train.ann_file=$3"
+                    "data.train.ann_file=$3" \
     --gpu-id=0
 
