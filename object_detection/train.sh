2337a1ca4098a75d5fc3545067bcb8f82425bd2c
adamcread <adamcread1@gmail.com>
2022-04-03 16:29:24 +0100

    update train for dbf3

--- object_detection/train.sh
+++ object_detection/train.sh
@@ -18,10 +18,10 @@ source /etc/profile
 module load cuda/11.0-cudnn8.0
 source ../venv/bin/activate
 
-if [ $1 = "dbf3_crcnn" ] || [ $1 = "dbf3_fsaf" ]
+if [ $1 = "dbf3" ]
 then
-    python3 tools/train.py './configs/custom/cascade_rcnn_config.py' \
-        --cfg-options   "work_dir=work_dirs/$1/" \
+    python3 tools/train.py "./configs/custom/$2_config.py" \
+        --cfg-options   "work_dir=work_dirs/$1/$2" \
         --gpu-id=0  
 else
     python3 tools/train.py  "./configs/custom/$2_config.py" \
