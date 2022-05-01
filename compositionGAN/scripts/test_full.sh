e5ccf5700616e5d0411dbc02399973c502e998f7
adamcread <adamcread1@gmail.com>
2022-05-01 13:47:48 +0100

    typo

--- compositionGAN/scripts/test_full.sh
+++ compositionGAN/scripts/test_full.sh
@@ -123,7 +123,7 @@ do
 		--cfg-options   "classwise=True" \
 						"work_dir=../object_detection/work_dirs/test/" \
 						"data.test.img_prefix=./results/${name}/test_${epoch}/images/" \
-						"data.test.ann_file=./results/${name}/annotation/test_${epoch}.json" 
+						"data.test.ann_file=./results/${name}/annotation/test_${epoch}.json" \
 		--epoch_count ${which_epoch} \
 		--update_html_freq ${update_html_freq} \
 		--display_freq ${display_freq} \
