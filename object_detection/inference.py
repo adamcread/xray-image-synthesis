1aee85a15bd4c279f06b328f461462bb7d1d256f
adamcread <adamcread1@gmail.com>
2022-05-01 13:34:08 +0100

    add inference

--- object_detection/inference.py
+++ object_detection/inference.py
@@ -1,13 +1,32 @@
 from mmcv import Config, mkdir_or_exist
 from mmdet.apis import inference_detector, init_detector, show_result_pyplot
 
-PREFIX = '../dataset/xray/unpaired/'
-cfg = Config.fromfile('./configs/custom/cascade_rcnn_config.py')
+PREFIX = './inference/original/'
+cfg = Config.fromfile('./configs/custom/crcnn_config.py')
 
-model = init_detector(cfg, checkpoint='checkpoints/epoch_6.pth', device='cpu')
+model = init_detector(cfg, checkpoint='./work_dirs/dbf3/crcnn/best.pth', device='cpu')
 model.CLASSES = cfg.classes
 
-img = PREFIX + 'original_size/composed_images/BAGGAGE_20140123_103927_68622_D.jpg'
-result = inference_detector(model, img)
-model.show_result(img, result, out_file='result.jpg')
+img_firearm = PREFIX + 'firearm.jpg'
+result_firearm = inference_detector(model, img_firearm)
+model.show_result(
+    img_firearm, 
+    result_firearm, out_file='./inference/result/firearm.jpg',
+    bbox_color=(255, 0, 0), 
+    text_color=(255, 0, 0), 
+    thickness=3, 
+    font_size=0
+)
+
+
+img_knife = PREFIX + 'knife.jpg'
+result_knife = inference_detector(model, img_knife)
+model.show_result(
+    img_knife, 
+    result_knife, out_file='inference/result/knife.jpg',
+    bbox_color=(255, 0, 0), 
+    text_color=(255, 0, 0), 
+    thickness=3, 
+    font_size=0
+)
 
