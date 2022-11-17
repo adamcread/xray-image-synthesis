1d8253da651388662eaec4b15d7755f9028a04c4
Adam Read <adamcread1@gmail.com>
2022-11-17 15:04:54 +0000

    clean repo

--- compositionGAN/scripts/test.sh
+++ compositionGAN/scripts/test.sh
@@ -19,23 +19,23 @@ source ../venv/bin/activate
 mode=test
 name="test"
 
-datalist_test="./scripts/test_demo.txt"
+datalist_test="./scripts/paths_test.txt"
 
-# if [ $1 = "paired" ]
-# then
-# 	dataset_mode="comp_decomp_aligned"
-# 	datalist="./scripts/paths_train_paired.txt"
-# elif [ $1 = "unpaired" ]
-# then
-# 	dataset_mode="comp_decomp_unaligned" # dataset type to choose model type
-# 	datalist="./scripts/paths_train_unpaired.txt"
-# fi
+if [ $1 = "paired" ]
+then
+	dataset_mode="comp_decomp_aligned"
+	datalist="./scripts/paths_train_paired.txt"
+elif [ $1 = "unpaired" ]
+then
+	dataset_mode="comp_decomp_unaligned" # dataset type to choose model type
+	datalist="./scripts/paths_train_unpaired.txt"
+fi
 
 exp_train="train_"$1"_"$2
 name="test_"$1"_"$2
 model_train="./checkpoints/${exp_train}/"
 
-how_many=$4
+how_many=$3
 batch_size=1
 
 loadSizeY=128
@@ -59,64 +59,71 @@ update_html_freq=5
 n_latest=1
 xray=true
 
+
+
 if [ ! -d "./checkpoints/${name}" ]; then
 	mkdir "./checkpoints/${name}"
 fi
 
-python3 move_checkpoints.py \
-	-i ${model_train} \
-	-e $3 \
-	-d "./checkpoints/${name}/"
+for epoch in {0..1000..50}
+do
+	python3 move_checkpoints.py \
+		-i ${model_train} \
+		-e ${epoch} \
+		-d "./checkpoints/${name}/"
 
 
-CUDA_LAUNCH_BLOCKING=${CUDA_ID} CUDA_VISIBLE_DEVICES=${CUDA_ID} \
-	python3 test_composition.py \
-	--datalist ${datalist} \
-	--datalist_test ${datalist_test} \
-	--xray ${xray} \
-	--decomp \
-	--name ${name} \
-	--dataset_mode ${dataset_mode} \
-	--no_flip \
-	--conditional \
-	--erosion \
-	--img_completion \
-	--lr ${LR} \
-	--lambda_L2 ${lambda_L2} \
-	--batchSize ${batch_size} \
-	--niter ${niter} \
-	--niter_decay ${niter_decay} \
-	--fineSizeY ${fineSizeY} \
-	--loadSizeY ${loadSizeY} \
-	--STN_model ${STN_model} \
-	--which_epoch ${EPOCH} \
-	--Thresh1 ${thresh1} \
-	--Thresh2 ${thresh2} \
-	--G1_completion ${G1_comp} \
-	--G2_completion ${G2_comp} \
-	--display_freq ${display_freq} \
-	--display_id 1 \
-	--display_port ${display_port} \
-	--print_freq ${print_freq} \
-	--update_html_freq ${update_html_freq} \
-	--n_latest ${n_latest} \
-	--how_many ${how_many} \
-	--eval
+	CUDA_LAUNCH_BLOCKING=${CUDA_ID} CUDA_VISIBLE_DEVICES=${CUDA_ID} \
+		python3 test_composition.py \
+		--datalist ${datalist} \
+		--datalist_test ${datalist_test} \
+		--xray ${xray} \
+		--decomp \
+		--name ${name} \
+		--dataset_mode ${dataset_mode} \
+		--no_flip \
+		--conditional \
+		--erosion \
+		--img_completion \
+		--lr ${LR} \
+		--lambda_L2 ${lambda_L2} \
+		--batchSize ${batch_size} \
+		--niter ${niter} \
+		--niter_decay ${niter_decay} \
+		--fineSizeY ${fineSizeY} \
+		--loadSizeY ${loadSizeY} \
+		--STN_model ${STN_model} \
+		--which_epoch ${EPOCH} \
+		--Thresh1 ${thresh1} \
+		--Thresh2 ${thresh2} \
+		--G1_completion ${G1_comp} \
+		--G2_completion ${G2_comp} \
+		--display_freq ${display_freq} \
+		--display_id 1 \
+		--display_port ${display_port} \
+		--print_freq ${print_freq} \
+		--update_html_freq ${update_html_freq} \
+		--n_latest ${n_latest} \
+		--how_many ${how_many} \
+		--eval
 
 
-rm -rf "./results/${name}/test_"$3"/"
-cp -R "./results/${name}/test_best/" "./results/${name}/test_"$3"/"
+	rm -rf "./results/${name}/test_"${epoch}"/"
+	cp -R "./results/${name}/test_best/" "./results/${name}/test_"${epoch}"/"
 
-python3 composed_dataset.py \
-	--file="${name}" \
-	--test="test_"$3
+	python3 composed_dataset.py \
+		--file="${name}" \
+		--test="test_"${epoch}
+
+	python3 ../object_detection/tools/test.py \
+		"../object_detection/configs/custom/crcnn_config.py" \
+		"../object_detection/work_dirs/dbf3/crcnn/best.pth" \
+		--eval "bbox" \
+		--cfg-options   "classwise=True" \
+						"work_dir=../object_detection/work_dirs/test/" \
+						"data.test.img_prefix=./results/${name}/test_${epoch}/images/" \
+						"data.test.ann_file=./results/${name}/annotation/test_${epoch}.json"
+done
 
 
-python3 ../object_detection/tools/test.py \
-    "../object_detection/configs/custom/crcnn_config.py" \
-    "../object_detection/work_dirs/dbf3/crcnn/best.pth" \
-    --eval "bbox" \
-    --cfg-options   "classwise=True" \
-                    "work_dir=../object_detection/work_dirs/test/" \
-                    "data.test.img_prefix=./results/${name}/test_$3/images/" \
-                    "data.test.ann_file=./results/${name}/annotation/test_$3.json" 
+ 
